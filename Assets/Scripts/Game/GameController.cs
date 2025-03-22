787c2329ad4b5a8fcf5d881267ce0e2b37b285ae
KOSMOGOR <mortalmortal2015@mail.ru>
2025-03-22 16:55:34 +0300

    add: DontDestroyOnLoad on GameController add: functions for enemy managment in GameController add: LevelController now pops enemies

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public static GameController i;
     public List<LevelInfo> levels;
+
+    public void Start() {
+        if (i) { Destroy(gameObject); return; }
+        i = gameObject.GetComponent<GameController>();
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public Dictionary<Enemy, int> PopEnemies(string sceneName) {
+        LevelInfo li = levels.Find(x => x.scene.name == sceneName);
+        Dictionary<Enemy, int> enemies = li.enemies;
+        li.enemies.Clear();
+        return enemies;
+    }
+
+    public void AddEnemyInLevel(Enemy enemy, string sceneName) {
+        Dictionary<Enemy, int> enemies = levels.Find(x => x.scene.name == sceneName).enemies;
+        if (enemies.ContainsKey(enemy)) enemies[enemy] += 1;
+        else enemies[enemy] = 1;
+    }
 }
 
 [Serializable]
 public class LevelInfo {
     public SceneAsset scene;
-    public List<EnemyInLevel> enemies;
-}
-
-[Serializable]
-public class EnemyInLevel {
-    public GameObject enemy;
-    public int count;
+    public Dictionary<Enemy, int> enemies;
 }
